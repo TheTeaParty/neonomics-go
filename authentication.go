ecd209c3a55f98d570ef961230c249d77df5db71
Vlad Gorokhov <vlad@witchery.io>
2022-07-01 02:01:43 +0400

    client init function added (#7)

---
+++ authentication.go
@@ -0,0 +1,13 @@
+package neonomics
+
+import "context"
+
+func (c *client) TokenRequest(ctx context.Context, req *TokenRequestRequest) (*TokenRequestResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) TokenRefresh(ctx context.Context, req *TokenRefreshRequest) (*TokenRefreshResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ consent.go
@@ -0,0 +1,8 @@
+package neonomics
+
+import "context"
+
+func (c *client) GetConsent(ctx context.Context, ID string) (*GetConsentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
