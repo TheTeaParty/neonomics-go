b48e8e6dee6ca1262f4e3a2bb73d2cebeef5c5bb
Vlad Gorokhov <vlad@witchery.io>
2022-06-30 23:38:57 +0400

    token request implementation

--- authentication.go
+++ authentication.go
@@ -1,10 +1,33 @@
 package neonomics
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
 func (c *client) TokenRequest(ctx context.Context, req *TokenRequestRequest) (*TokenRequestResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
+	requestBody, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+
+	responseBody, err := c.doRequest(ctx, PathTokenRequest, http.MethodPost, headers, requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	var response *TokenRequestResponse
+	if err := json.Unmarshal(responseBody, response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
 
 func (c *client) TokenRefresh(ctx context.Context, req *TokenRefreshRequest) (*TokenRefreshResponse, error) {
