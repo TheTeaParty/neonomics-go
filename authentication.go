c1be9dbfbb629c4670221d48857bf7c8268df42f
Vlad Gorokhov <vlad@witchery.io>
2022-07-01 00:46:45 +0400

    refresh token

--- authentication.go
+++ authentication.go
@@ -47,6 +47,41 @@ func (c *client) TokenRequest(ctx context.Context) (*TokenResponse, error) {
 }
 
 func (c *client) TokenRefresh(ctx context.Context) (*TokenResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
+	req := &TokenRefreshRequest{
+		GrantType:    "client_credentials",
+		ClientId:     c.config.ClientID,
+		ClientSecret: c.config.SecretID,
+		RefreshToken: c.tokenResponse.RefreshToken,
+	}
+
+	data := url.Values{}
+	data.Set("grant_type", req.GrantType)
+	data.Set("client_id", req.ClientId)
+	data.Set("client_secret", req.ClientSecret)
+	data.Set("refresh_token", req.RefreshToken)
+
+	headers := map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+
+	reqURL := fmt.Sprintf(string(PathTokenRequest), "sandbox")
+	if c.backend.Environment == EnvironmentProduction {
+		reqURL = fmt.Sprintf(string(PathTokenRequest), "live")
+	}
+
+	responseBody, err := c.doRequest(ctx, reqURL, http.MethodPost, headers, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	var response TokenResponse
+	if err := json.Unmarshal(responseBody, &response); err != nil {
+		return nil, err
+	}
+
+	c.tokenResponse = &response
+
+	return &response, nil
+
 }
