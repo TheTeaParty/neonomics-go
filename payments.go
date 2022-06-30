ada58794ffcb6b73b0771729815e4de5a3b3d90d
Vlad Gorokhov <vlad@witchery.io>
2022-06-30 23:00:07 +0400

    adding missing methods

--- payments.go
+++ payments.go
@@ -1,1 +1,38 @@
 package neonomics
+
+import "context"
+
+func (c *client) DomesticPayment(ctx context.Context, req *DomesticPaymentRequest) (*DomesticPaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) DomesticScheduledPayment(ctx context.Context, req *DomesticScheduledPaymentRequest) (*DomesticScheduledPaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) SEPAPayment(ctx context.Context, req *SEPAPaymentRequest) (*SEPAPaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) SEPAScheduledPayment(ctx context.Context, req *SEPAScheduledPaymentRequest) (*SEPAScheduledPaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) GetPaymentByID(ctx context.Context, ID string) (*GetPaymentByIDResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) AuthorizePayment(ctx context.Context, paymentProduct, paymentId string) (*AuthorizePaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *client) CompletePayment(ctx context.Context, paymentProduct, paymentId string) (*CompletePaymentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
